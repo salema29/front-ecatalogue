31f39c6542774ecc75d6a6f07bb11ec7a1170f62
Salema ANDRIAMASY <15556@madcom.local>
2024-11-20 17:12:23 +0300

    GUT-26 : build files name with craco

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'app.ecatalogue.com',
-  appName: 'ecatalogue-mobile',
-  webDir: 'build'
-};
-
-export default config;
