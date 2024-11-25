20e28ecfff823c0dd2abcd03a57b55acd89247a3
Fiderana Ny Avo RAMAHEFENOSOA <15596@madcom.local>
2024-11-25 15:08:43 +0300

    GUT-26 :construction: Dynamisation de la vue detaille

--- src/components/navigation/CategoryMenu.js
+++ src/components/navigation/CategoryMenu.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from "react-router-dom";
-import '../../assets/styles/CategoryMenu.css'; 
+import { useParams, useNavigate } from "react-router-dom";
+import '../../assets/styles/CategoryMenu.css';
 
 const CircularIconsRow = () => {
     const { catalogId } = useParams();
     const [categoryList, setCategoryList] = useState([]);
-    
+    const navigate = useNavigate();
+
+    const handleCategoryProductsView = (category) => {
+        navigate(`/products/${category}`);
+        // Perform actions with the category data
+    };
+
     useEffect(() => {
         fetch(`http://localhost/admin-ecatalogue-v2/api/getCategory/${catalogId}`)
             .then(response => {
@@ -20,21 +26,38 @@ const CircularIconsRow = () => {
             });
     }, [catalogId]);
 
+    console.log(categoryList);
     return (
         <div>
             {categoryList.length > 0 ? (
                 <div className="circular-icons-row">
                     {categoryList.map((category, index) => (
-                        <div className="icon-container" key={category.categorie_id} height="186px">
-                            <a href={category.categorie_image}>
-                                <img src={category.categorie_image} alt={category.categorie_name} className="icon" />
-                            </a>
+                        <div
+                            className="icon-container"
+                            key={category.categorie_id}
+                            height="186px"
+                        >
+                            <div onClick={handleCategoryProductsView.bind(null, category.categorie_id)}>
+                                <img
+                                    src={category.categorie_image}
+                                    alt={category.categorie_name}
+                                    className="icon"
+                                />
+                            </div>
                             <p className="icon-label">{category.categorie_name}</p>
+                            <input
+                                type="hidden"
+                                id={`category-${category.categorie_id}`}  // Dynamic ID
+                                name="categoryId"
+                                value={category.categorie_id}
+                            />
                         </div>
                     ))}
                 </div>
             ) : (
-                <p> Il n'y a pas de catégorie disponible </p>
+                <>
+                    {/* <p> Il n'y a pas de catégorie disponible </p> */}
+                </>
             )}
         </div>
     );
