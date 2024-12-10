f0322a84a8b657122197c4ede553fc1a59e658af
Harena Juan RABARIJAONA <17491@madcom.local>
2024-12-10 14:42:12 +0300

    Gérer l'affichage des flèches de navigation suivant le nombre de catalogue

---
+++ src/components/slide.data.js
@@ -0,0 +1,50 @@
+export const dataSlide = [
+    {
+        "catalogue_id": "359",
+        "catalogue_client_id": "189",
+        "catalogue_name_ln_un": "noel",
+        "catalogue_name_ln_deux": "",
+        "slide_image": "http://localhost/front-ecatalogue-v2/clients/weldom-local/Slides/catalogue_359/VIGNETTE-Noel-1.jpg",
+        "catalogue_date_validite_debut": "05 Décembre 2024",
+        "catalogue_date_validite_fin": "22 Décembre 2024",
+        "catalogue_link": "http://localhost/front-ecatalogue-v2/clients/weldom-local/noel/index.html",
+        "client_logo": "http://localhost/front-ecatalogue-v2/clients/weldom-local/Client_logo/logo_gun.png",
+        "client_color": "#11afdf"
+    },
+    {
+        "catalogue_id": "359",
+        "catalogue_client_id": "189",
+        "catalogue_name_ln_un": "noel",
+        "catalogue_name_ln_deux": "",
+        "slide_image": "http://localhost/front-ecatalogue-v2/clients/weldom-local/Slides/catalogue_359/VIGNETTE-Noel-1.jpg",
+        "catalogue_date_validite_debut": "05 Décembre 2024",
+        "catalogue_date_validite_fin": "22 Décembre 2024",
+        "catalogue_link": "http://localhost/front-ecatalogue-v2/clients/weldom-local/noel/index.html",
+        "client_logo": "http://localhost/front-ecatalogue-v2/clients/weldom-local/Client_logo/logo_gun.png",
+        "client_color": "#11afdf"
+    },
+    {
+        "catalogue_id": "359",
+        "catalogue_client_id": "189",
+        "catalogue_name_ln_un": "noel",
+        "catalogue_name_ln_deux": "",
+        "slide_image": "http://localhost/front-ecatalogue-v2/clients/weldom-local/Slides/catalogue_359/VIGNETTE-Noel-1.jpg",
+        "catalogue_date_validite_debut": "05 Décembre 2024",
+        "catalogue_date_validite_fin": "22 Décembre 2024",
+        "catalogue_link": "http://localhost/front-ecatalogue-v2/clients/weldom-local/noel/index.html",
+        "client_logo": "http://localhost/front-ecatalogue-v2/clients/weldom-local/Client_logo/logo_gun.png",
+        "client_color": "#11afdf"
+    },
+    {
+        "catalogue_id": "359",
+        "catalogue_client_id": "189",
+        "catalogue_name_ln_un": "noel",
+        "catalogue_name_ln_deux": "",
+        "slide_image": "http://localhost/front-ecatalogue-v2/clients/weldom-local/Slides/catalogue_359/VIGNETTE-Noel-1.jpg",
+        "catalogue_date_validite_debut": "05 Décembre 2024",
+        "catalogue_date_validite_fin": "22 Décembre 2024",
+        "catalogue_link": "http://localhost/front-ecatalogue-v2/clients/weldom-local/noel/index.html",
+        "client_logo": "http://localhost/front-ecatalogue-v2/clients/weldom-local/Client_logo/logo_gun.png",
+        "client_color": "#11afdf"
+    }
+];
