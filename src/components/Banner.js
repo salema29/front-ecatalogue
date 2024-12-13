dda45f038c9b5d6fca811ac7ce4d42297ccf2c1b
Harena Juan RABARIJAONA <17491@madcom.local>
2024-12-13 11:45:51 +0300

    GUT : 26 - Dynamisation banniere

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,27 +4,29 @@ import '../assets/styles/Banner.css'
 function Banner() {
     const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
     const [client_id, setClientId] = useState(null);
+    const [bannerMedia, setBannerMedia] = useState('');
     const client = document.getElementById('catalogue-client');
-    // const [slidesData, setSlidesData] = useState([]);
-    // useEffect(() => {
-    //     const fetchedValue = client ? client.value : null;
-    //     setClientId(fetchedValue);
 
-    //     // fetch(`${API_BASE_URL}/api/getDataClient/${client}`)
-    //     fetch(`${API_BASE_URL}/api/getDataClient/186`) //Static data for testing
-    //         .then((response) => response.json())
-    //         .then((fetchedData) => setSlidesData(fetchedData))
-    //         .catch((error) => console.error("Error fetching data:", error));
-    // }, client_id);
+    useEffect(() => {
+        const fetchedValue = client ? client.value : null;
+        setClientId(fetchedValue);
+    }, []);
 
-    const BannerMedia = 'https://preprod-appli-server.vivetic.com/web_si/front-ecatalogue-v2/assets/images/ecata-header.webp';
+    useEffect(() => {
+        fetch(`${API_BASE_URL}/api/getClientImage/${client_id}`)
+        // fetch(`${API_BASE_URL}/api/getClientImage/194`) // Static data for testing
+            .then((response) => response.json())
+            .then((fetchedData) => setBannerMedia(fetchedData.client_global_image))
+            .catch((error) => console.error("Error fetching data:", error));
+
+    }, client_id);
 
     return (
         <div className='section-banner'>
             <img
                 alt='ecatalogue header media'
                 className='banner-media'
-                src={BannerMedia}
+                src={bannerMedia}
             />
             <h1 className='banner-text'>Nos catalogues</h1>
         </div>
