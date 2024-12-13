26abd6c682e742b9446fc624d561ea3c8ebe6ba0
Salema ANDRIAMASY <salema.andriamasy@vivetic.mg>
2024-12-13 10:46:42 +0300

    GUT-43 : dynamic client image

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,16 +7,13 @@ function Home() {
     const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
     const [clientBannerMedia, setClientBannerMedia] = useState(null);
     let environment_shop_id = 0;
-
     if (window.dataLayer && window.dataLayer[0]?.cdl_environment_shop) {
         environment_shop_id = window.dataLayer[0].cdl_environment_shop;
     }
     const client = document.getElementById('catalogue-client');
-
     useEffect(() => {
         fetchSlideData();
     }, [client, environment_shop_id, API_BASE_URL]);
-
     const fetchSlideData = () => {
         // fetch(`${API_BASE_URL}/api/getSlides/190/0`) //Static data for testing
         fetch(`${API_BASE_URL}/api/getSlides/${inputValue}/${shopId}`)
@@ -33,7 +30,6 @@ function Home() {
             console.error('Error fetching data:', error);
         });
     }
-
     return (
         <>
             <Banner clientBannerMedia={clientBannerMedia}/>
@@ -41,5 +37,4 @@ function Home() {
         </>
     );
 };
-
 export default Home;
