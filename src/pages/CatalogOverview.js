9a9159e8c0e83b9239a7c0dd7603c78770908ba8
Salema ANDRIAMASY <salema.andriamasy@vivetic.mg>
2025-01-09 17:08:14 +0300

    GUT-51 : espace entre les dates dans la vue resumée

--- src/pages/CatalogOverview.js
+++ src/pages/CatalogOverview.js
@@ -45,7 +45,7 @@ function Catalog() {
                   {headerData.catalogue_name_ln_deux}
                 </p>
                 <p style={{ color: "black" }}>
-                  du {headerData.catalogue_date_validite_debut} au{" "}
+                  du {headerData.catalogue_date_validite_debut} au {" "}
                   {headerData.catalogue_date_validite_fin}
                 </p>
               </div>
