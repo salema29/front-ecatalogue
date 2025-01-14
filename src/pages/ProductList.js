e328ec93f2eca136fd95a6a15ffd14646aea9963
Harena Juan RABARIJAONA <17491@madcom.local>
2025-01-14 11:09:11 +0300

    GUT-57 : push productList

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -3,6 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import CategoryMenu from "../components/navigation/CategoryMenu";
 import LoadingSpinner from '../components/spinner/LoadingSpinner'
 import "../assets/styles/ProductList.css";
+import cross from "../assets/icons/cross-icon-dark.svg";
 
 function Product() {
     const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
@@ -12,6 +13,8 @@ function Product() {
     const [productData, setProductData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 767);
+    const headerHeight = document.querySelector('.sticky'); // class "sticky" height
+    const [wrapperHeight, setWrapperHeight] = useState(window.innerHeight - headerHeight); 
     const navigate = useNavigate();
 
     const handleClose = () => {
@@ -23,7 +26,7 @@ function Product() {
     };
 
     const handleDetailedView = (product_id) => {
-        navigate(`/product/${catalogId}/${product_id}`);
+        navigate(`/product/${catalogId}/${product_id}/${categoryId}`);
     };
     // Gerer le media query pour la mise en page responsive du grille desktop/moble
     useEffect(() => {
@@ -89,6 +92,26 @@ function Product() {
     }, [categoryId, API_BASE_URL]);
 
     useEffect(() => {
+        if (headerData) {
+            const updateHeight = () => {
+                const stickyElement = document.querySelector('.sticky');
+                const stickyHeight = stickyElement ? stickyElement.getBoundingClientRect().height : 0;
+
+                const height = window.innerHeight - stickyHeight;
+                setWrapperHeight(height);
+            };
+    
+            setTimeout(updateHeight, 1000); // Assurez-vous que le DOM est à jour.
+            window.addEventListener('resize', updateHeight);
+    
+            return () => {
+                window.removeEventListener('resize', updateHeight);
+            };
+        }
+    }, [headerData]);
+
+
+    useEffect(() => {
         const wrapper = document.querySelector('.wrapper');
         if (wrapper) {
           wrapper.scrollTop = 0; // Réinitialise le scroll de l'élément wrapper
@@ -105,6 +128,7 @@ function Product() {
                                 <img
                                     className="header-logo"
                                     src={headerData.client_logo}
+                                    // src="https://v2.ecatalogues.fr/clients/Gutenberg/Client_logo/logo_gut_noir.png"
                                     alt=""
                                 />
                                 <div className="header-text">
@@ -154,7 +178,12 @@ function Product() {
                             (<></>)
                         }
                     </div>
-                    <div className="wrapper">
+                    <div className="wrapper"
+                        style={{
+                            height: `${wrapperHeight}px`, 
+                            overflowY: 'scroll'
+                        }}
+                    >
                         <div className="product-list-container" >
                             {isLoading === false && (
                                 productData.length > 0 ?
@@ -163,8 +192,7 @@ function Product() {
                                             {
                                                 productData.map((product, index) => {
                                                     // Retourne uniquement les produit de type vue resumé
-                                                    if (product.view_type === '1' )
-                                                    {
+                                                    if (product.view_type === '1') {
                                                         return (
                                                             <div
                                                                 key={index}
@@ -178,7 +206,7 @@ function Product() {
                                                                     {product.type == 0 ?
                                                                         (   <div className="item-wrapper">
                                                                                 <div
-                                                                                    onClick={() => handleDetailedView(product.view_order)}
+                                                                                    onClick={() => handleDetailedView(product.view_order, product.categoryId)}
                                                                                     className="item-link"
                                                                                     style={{
                                                                                         cursor: 'pointer',
@@ -203,10 +231,10 @@ function Product() {
                                                                                     className="product-item"
                                                                                     title={`Product ${index}`}
                                                                                 />
-                                                                                </div>
                                                                             </div>
-                                                                        )
-                                                                    }
+                                                                        </div>
+                                                                    )
+                                                                }
                                                             </div>
                                                         );
                                                     }
@@ -223,7 +251,7 @@ function Product() {
                                         </div>
                                     )
                             )}
-                            {isLoading === true && (  <LoadingSpinner /> )}
+                            {isLoading === true && (<LoadingSpinner />)}
                         </div>
                     </div>
                 </div>
