7195bc209abcfe933ce52a2ae1012eeb04b9abd7
Salema ANDRIAMASY <salema.andriamasy@vivetic.mg>
2025-03-13 10:49:47 +0300

    GUT-86 : useContext vue détaille

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import LoadingSpinner from '../components/spinner/LoadingSpinner'
 import '../assets/styles/ProductDetail.css';
