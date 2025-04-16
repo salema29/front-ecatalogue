9df4c5f146e585a90d8c3a91595715863fa00f45
Salema ANDRIAMASY <salema.andriamasy@vivetic.mg>
2025-04-16 10:46:37 +0300

    GUT-99 : hide  ate in the header on small screen, logo according screen size

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -159,11 +159,11 @@ function Product() {
                                     alt=""
                                 />
                                 <div className="header-text">
-                                    <p style={{ color: headerData ? headerData.client_color : "#fff" }} >
+                                    <p className="catalogue-name" style={{ color: headerData ? headerData.client_color : "#fff" }} >
                                         {headerData.catalogue_name_ln_un}{" "}
                                         {headerData.catalogue_name_ln_deux}
                                     </p>
-                                    <p style={{ color: "black" }}>
+                                    <p className="catalogue-date"style={{ color: "black" }}>
                                         du {headerData.catalogue_date_validite_debut} au{" "}
                                         {headerData.catalogue_date_validite_fin}
                                     </p>
