1665c7f7a20f26e8348f7f6e4d7386b1eed4ea05
Harena Juan RABARIJAONA <17491@madcom.local>
2025-06-16 12:52:26 +0300

    GUT-120 :lipstick: update icon

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext  } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import CategoryMenu from "../components/navigation/CategoryMenu";
 import LoadingSpinner from '../components/spinner/LoadingSpinner';
@@ -108,7 +108,7 @@ function Product() {
     }, [categoryId, API_BASE_URL, catalogId]);
 
     useEffect(() => {
-        if (headerData && categoryList ) {
+        if (headerData && categoryList) {
             const updateHeight = () => {
                 const stickyElement = document.querySelector('.sticky');
                 const stickyHeight = stickyElement ? stickyElement.getBoundingClientRect().height : 0;
@@ -161,7 +161,7 @@ function Product() {
     };
 
     useEffect(() => {
-        if (searchQuery || searchResults ) {
+        if (searchQuery || searchResults) {
             const updateHeightResultatSearch = () => {
                 const stickyElement = document.querySelector('.sticky');
                 const stickyHeight = stickyElement ? stickyElement.getBoundingClientRect().height : 0;
@@ -205,7 +205,7 @@ function Product() {
                                 </div>
                             </div>
                             <div className="view-format-dialog-right-part">
-                                {!isMobileView && productData.length > 0 ? (<SearchBar onResults={handleSearchResults}  catalogue_id={catalogId} onQueryChange={handleQueryChange} />) : (<></>)}
+                                {!isMobileView && productData.length > 0 ? (<SearchBar onResults={handleSearchResults} catalogue_id={catalogId} onQueryChange={handleQueryChange} />) : (<></>)}
 
                                 {viewChoice.isVueFeuilletable && (
                                     <button
@@ -217,13 +217,13 @@ function Product() {
                                             height: "25",
                                             width: "auto"
                                         }}>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="51" height="30" fill="none"><g stroke={headerData ? headerData.client_color : "#fff"} strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.401" clipPath="url(#a)">
-                                                <path d="M5.356 21.495h12.382c5.8 0 7.09 5.796 7.09 5.796V6.496S23.538.701 17.738.701H5.356v20.794ZM44.724 21.495H31.956c-5.8 0-7.09 5.796-7.09 5.796V6.496s1.29-5.795 7.09-5.795h12.768v20.794Z" />
-                                                <path d="M24.866 28.096h-.038s-1.29-4.569-7.09-4.569H2.94V2.694" />
-                                                <path d="M24.866 29.3h-.038s-1.29-3.548-7.09-3.548H.701V4.854M24.942 28.096h.038s1.289-4.569 7.09-4.569h15.013V2.694" />
-                                                <path d="M24.942 29.3h.038s1.289-3.548 7.09-3.548h17.25V4.854" /></g><defs><clipPath id="a">
-                                                    <path fill="#fff" d="M0 0h50.021v30H0z" /></clipPath></defs>
+                                            <svg width="33" height="29" viewBox="0 0 46 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M3.73762 7.2074C2.91199 7.54057 1 8.4787 1 8.4787V38.6393C1 38.6393 12.7153 33.9399 23.04 40.0948" stroke={headerData.client_color ? headerData.client_color : "#164194"} stroke-width="1.48571" stroke-linecap="round" stroke-linejoin="round" />
+                                                <path d="M23.04 9.94298C16.9477 5.12079 11.5159 6.78663 6.91846 1V31.1694C11.5159 36.956 16.9477 35.2814 23.04 40.1036" stroke={headerData.client_color ? headerData.client_color : "#164194"} stroke-width="1.48571" stroke-linecap="round" stroke-linejoin="round" />
+                                                <path d="M23.04 9.94319C34.0601 2.50825 45.0801 8.48777 45.0801 8.48777V38.6484C45.0801 38.6484 33.3648 33.9577 23.04 40.1038" stroke={headerData.client_color ? headerData.client_color : "#164194"} stroke-width="1.48571" stroke-linecap="round" stroke-linejoin="round" />
+                                                <path d="M23.04 9.94293V40.1036" stroke={headerData.client_color ? headerData.client_color : "#164194"} stroke-width="1.48571" stroke-linecap="round" stroke-linejoin="round" />
                                             </svg>
+
                                         </span>
                                     </button>
                                 )}
@@ -252,16 +252,16 @@ function Product() {
                                 </button>
                             </div>
                         </header>
-                        { productData.length > 0 && (
-                            <div style={{ backgroundColor: "white"}}>
-                                {isMobileView && (<SearchBar onResults={handleSearchResults}  catalogue_id={catalogId} onQueryChange={handleQueryChange} />)}
+                        {productData.length > 0 && (
+                            <div style={{ backgroundColor: "white" }}>
+                                {isMobileView && (<SearchBar onResults={handleSearchResults} catalogue_id={catalogId} onQueryChange={handleQueryChange} />)}
                                 {!searchQuery ? (
                                     <CategoryMenu categoryIdSelected={categoryId} categoryList={categoryList} />)
                                     :
                                     (
                                         <button className="search-return btn" title="Retour" onClick={returnToCategory}>
                                             <svg className="search-return-icon" xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" style={{ transform: "rotate(180deg)" }}>
-                                                <path fill="#414141" fill-rule="evenodd" d="m16.334 10.999-6.562 6.55 1.416 1.414 8.27-8.258.709-.706-.708-.707-8.271-8.257-1.416 1.413 6.562 6.551H0v2h16.334Z" clip-rule="evenodd"/></svg>
+                                                <path fill="#414141" fill-rule="evenodd" d="m16.334 10.999-6.562 6.55 1.416 1.414 8.27-8.258.709-.706-.708-.707-8.271-8.257-1.416 1.413 6.562 6.551H0v2h16.334Z" clip-rule="evenodd" /></svg>
                                             <span>Retour </span>
                                         </button>
                                     )
@@ -275,73 +275,74 @@ function Product() {
                             overflowY: 'scroll'
                         }}
                     >
-                            {searchQuery ?
-                                // si on cherhce qlq chose
-                                (
-                                    <div className="product-list-container">
-                                        {loading ?
+                        {searchQuery ?
+                            // si on cherhce qlq chose
+                            (
+                                <div className="product-list-container">
+                                    {loading ?
+                                        (
+                                            <LoadingSpinner />
+                                        ) : searchResults.length > 0 ? (
+                                            <div className="grid-container">
+                                                {searchResults.map((product, index) => (
+                                                    <ProductItem
+                                                        key={product.id_produit || index}
+                                                        product={product}
+                                                        index={index}
+                                                        categoryId={product.product_categorie_id}
+                                                        catalogId={catalogId}
+                                                        showListCourse="t"
+                                                        clientColor={headerData.client_color}
+                                                    />
+                                                ))}
+                                            </div>
+                                        ) : (
+                                            <div className="empty-search-result" >
+                                                <p style={{ color: headerData.client_color }}>
+                                                    Aucun produit trouvé
+                                                </p>
+                                            </div>
+                                        )
+                                    }
+                                </div>
+                            ) :
+                            // si on consulte par categorie
+                            (
+                                <div className="product-list-container">
+                                    {isLoading ? (
+                                        <LoadingSpinner />
+                                    ) : (
+                                        productData.length > 0 ?
                                             (
-                                                <LoadingSpinner />
-                                            ) : searchResults.length > 0 ? (
                                                 <div className="grid-container">
-                                                    {searchResults.map((product, index) => (
-                                                        <ProductItem
-                                                            key={product.id_produit || index}
-                                                            product={product}
-                                                            index={index}
-                                                            categoryId={product.product_categorie_id}
-                                                            catalogId={catalogId}
-                                                            showListCourse="t"
-                                                            API_BASE_URL={API_BASE_URL}
-                                                        />
+                                                    {productData.map((product, index) => (
+                                                        product.view_type === '1' ? (
+                                                            <ProductItem
+                                                                key={index}
+                                                                product={product}
+                                                                index={index}
+                                                                categoryId={categoryId}
+                                                                catalogId={catalogId}
+                                                                showListCourse={headerData.show_list_course}
+                                                                clientColor={headerData.client_color}
+                                                            />
+                                                        ) : null
                                                     ))}
                                                 </div>
                                             ) : (
                                                 <div className="empty-search-result" >
                                                     <p style={{ color: headerData.client_color }}>
-                                                        Aucun produit trouvé
+                                                        Aucun produit disponible
                                                     </p>
                                                 </div>
                                             )
-                                        }
-                                    </div>
-                                ) :
-                                // si on consulte par categorie
-                                (
-                                    <div className="product-list-container">
-                                        { isLoading ? (
-                                            <LoadingSpinner />
-                                            ):(
-                                                productData.length > 0 ?
-                                                (
-                                                    <div className="grid-container">
-                                                        {productData.map((product, index) => (
-                                                            product.view_type === '1' ? (
-                                                                <ProductItem
-                                                                    key={index}
-                                                                    product={product}
-                                                                    index={index}
-                                                                    categoryId={categoryId}
-                                                                    catalogId={catalogId}
-                                                                    showListCourse={headerData.show_list_course}
-                                                                    API_BASE_URL={API_BASE_URL} />
-                                                            ) : null
-                                                        ))}
-                                                    </div>
-                                                ) : (
-                                                    <div className="empty-search-result" >
-                                                        <p style={{ color: headerData.client_color }}>
-                                                            Aucun produit disponible
-                                                        </p>
-                                                    </div>
-                                                )
-                                            )
-                                        }
-                                    </div>
-                                )
-                            }
+                                    )
+                                    }
+                                </div>
+                            )
+                        }
                         {/* </div> */}
-                        {modalOpen && ( <ShoppingListModal catalogId ={catalogId} isOpen={modalOpen} onClose={() => setModalOpen(false)} clientColor = {headerData.client_color} /> )}
+                        {modalOpen && (<ShoppingListModal catalogId={catalogId} isOpen={modalOpen} onClose={() => setModalOpen(false)} clientColor={headerData.client_color} />)}
                     </div>
                 </div>
             ) : (
